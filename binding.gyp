6c0016d8b3420037831a1494cedf9978f0529832
Matthew Keil <malibuss67@gmail.com>
2020-05-05 11:32:52 -0400

    found before hd backup

--- binding.gyp
+++ binding.gyp
@@ -2,13 +2,20 @@
   'targets': [
     {
       'target_name': 'native-modules-coursework',
-      'sources': [ 
-        'src/addon.cc'
+      'sources': ['src/addon.cc'],
+      'cflags!': [
+        '-fno-exceptions'
+        '-I/usr/local/opt/openssl/include',
+        '-L/usr/local/opt/openssl/lib',
+        '-lssl'
       ],
+      'cflags_cc!': [ '-fno-exceptions' ],
       'include_dirs': ["<!@(node -p \"require('node-addon-api').include\")"],
       'dependencies': ["<!(node -p \"require('node-addon-api').gyp\")"],
-      'cflags!': [ '-fno-exceptions' ],
-      'cflags_cc!': [ '-fno-exceptions' ],
+      'link_settings': {
+        'libraries': [ '-lopenssl' ],
+        'library_dirs': [ '/usr/local/opt/openssl/include' ]
+      }
       'xcode_settings': {
         'GCC_ENABLE_CPP_EXCEPTIONS': 'YES',
         'CLANG_CXX_LIBRARY': 'libc++',
--- src/section-5.h
+++ src/section-5.h
@@ -1,25 +1,42 @@
 #include <napi.h>
+#include <openssl/sha.h>
 
 class NativeClass : Napi::ObjectWrap<NativeClass>
 {
 public:
     static void Init(Napi::Env env, Napi::Object &exports);
     NativeClass(const Napi::CallbackInfo &info);
+    ~NativeClass();
 
 private:
     static Napi::FunctionReference constructor;
-    std::string opaque_token;
+    static unsigned char *opaque_token;
     Napi::Value DoSomthing(const Napi::CallbackInfo &info);
 };
+
+Napi::FunctionReference NativeClass::constructor;
 
 void NativeClass::Init(Napi::Env env, Napi::Object &exports)
 {
     Napi::HandleScope scope(env);
+    const unsigned char *id_input = "sha1 unique object id";
+    SHA1(id_input, strlen(reinterpret_cast<const char *>(id_input)), opaque_token);
+
     Napi::Function func = DefineClass(
         env,
         "NativeClass",
         {InstanceMethod("instanceMethod", &NativeClass::DoSomthing)});
+
     constructor = Napi::Persistent(func);
     constructor.SuppressDestruct();
     exports.Set(Napi::String::New(env, "section5"), func);
+}
+
+NativeClass::NativeClass(const Napi::CallbackInfo &info) : Napi::ObjectWrap<NativeClass>(info)
+{
+    Napi::Env env = info.Env();
+}
+
+NativeClass::~NativeClass()
+{
 }
